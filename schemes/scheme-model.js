8eef084373bfbf88c6c2811d1041a87538c73b46
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-18 12:32:47 +0100

    Add, Update and remove functions added

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,8 +5,33 @@ const find = () => db('schemes');
 
 const findByID = (id) => db('schemes').where({ id });
 
-const findSteps = (id) => db('steps').select(['id', 'scheme_name', 'step_number', 'instructions']).where({ scheme_id: id });
+const findSteps = (id) => {
+    return db
+        .select(['id', 'schemes.scheme_name', 'step_number', 'instructions'])
+        .from('steps')
+        .join('schemes')
+        .on('steps.scheme_id', 'schemes.id')
+        .where('steps.scheme_id', id );
+};
 
 const add = (scheme) => {
-    
+    return db('schemes')
+        .insert(scheme)
+        .then(ids => findByID(ids[0]));
+}
+
+const update = (changes, id) => {
+    return db('schemes')
+        .where({ id: id})
+        .update(changes)
+        .then(() => findByID(id));
+}
+
+const remove = async (id) => {
+    const scheme = await findByID(id);
+    if (!scheme) return null;
+    return db('schemes')
+        .where({ id: id })
+        .del()
+        .then(() => scheme);
 }
